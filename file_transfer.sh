3693355bebc2a920f747512b6ee4a87de5c65a26
N4RMA <manar-yacine@outlook.com>
2023-04-25 21:35:28 -0400

    changed file share webservice used to keep.sh

--- file_transfer.sh
+++ file_transfer.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+# Check if the file path argument is provided
 if [ $# -eq 0 ]; then
-  echo "Please provide a filename as an argument."
+  echo "Please provide a file path as an argument"
   exit 1
 fi
 
-filename=$1
-
-if [ ! -f $filename ]; then
-  echo "File not found."
+# Check if the file exists
+if [ ! -f $1 ]; then
+  echo "File not found"
   exit 1
 fi
 
-output=$(curl -s bashupload.com -T $filename)
+# Upload the file to keep.sh and get the link
+link=$(curl --upload-file $1 https://free.keep.sh 2>/dev/null)
 
-echo "$output" | awk '/wget/ {print}'
+# Check if the upload was successful
+if [ $? -ne 0 ]; then
+  echo "Upload failed"
+  exit 1
+fi
+
+# Print the link
+echo "Link: $link"
 
